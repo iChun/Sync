495b95a1fd3518139713a93cf920d4f20a604568
iChun <ohai.iChun@gmail.com>
2017-10-01 21:58:23 +0800

    Update to match iChunUtil's changes.

---
+++ src/main/java/me/ichun/mods/sync/common/core/RecipeHardcoreFactory.java
@@ -0,0 +1,21 @@
+package me.ichun.mods.sync.common.core;
+
+import com.google.gson.JsonObject;
+import me.ichun.mods.ichunutil.common.recipe.ConditionalIngredient;
+import me.ichun.mods.sync.common.Sync;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.common.DimensionManager;
+import net.minecraftforge.common.crafting.IIngredientFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+
+import javax.annotation.Nonnull;
+
+public class RecipeHardcoreFactory implements IIngredientFactory
+{
+    @Nonnull
+    @Override
+    public Ingredient parse(JsonContext context, JsonObject json)
+    {
+        return ConditionalIngredient.parseWithCondition(() -> Sync.config.hardcoreMode == 1 || Sync.config.hardcoreMode == 2 && DimensionManager.getWorld(0).getWorldInfo().isHardcoreModeEnabled(), context, json);
+    }
+}
