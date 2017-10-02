9518fd337e1d1ca8a522806cd8379bfa83fb91fe
Tobias Hotz <tobias.hotz@hotmail.de>
2017-10-02 19:34:09 +0200

    fix #191

--- src/main/java/me/ichun/mods/sync/common/core/RecipeHardcoreFactory.java
+++ src/main/java/me/ichun/mods/sync/common/core/RecipeHardcoreFactory.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 import me.ichun.mods.ichunutil.common.recipe.ConditionalIngredient;
 import me.ichun.mods.sync.common.Sync;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.world.World;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.crafting.IIngredientFactory;
 import net.minecraftforge.common.crafting.JsonContext;
@@ -16,6 +17,9 @@ public class RecipeHardcoreFactory implements IIngredientFactory
     @Override
     public Ingredient parse(JsonContext context, JsonObject json)
     {
-        return ConditionalIngredient.parseWithCondition(() -> Sync.config.hardcoreMode == 1 || Sync.config.hardcoreMode == 2 && DimensionManager.getWorld(0).getWorldInfo().isHardcoreModeEnabled(), context, json);
+        return ConditionalIngredient.parseWithCondition(() -> {
+            World world = DimensionManager.getWorld(0);
+            return Sync.config.hardcoreMode == 1 || Sync.config.hardcoreMode == 2 && (world == null || world.getWorldInfo().isHardcoreModeEnabled());
+        }, context, json);
     }
 }
