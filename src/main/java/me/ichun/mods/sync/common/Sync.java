801b1a31cd2b80a716f806f902499271f162d30e
iChun <ohai.iChun@gmail.com>
2017-12-12 20:36:12 +0800

    Use constants, update version requirement.

--- src/main/java/me/ichun/mods/sync/common/Sync.java
+++ src/main/java/me/ichun/mods/sync/common/Sync.java
@@ -40,7 +40,7 @@ import java.util.List;
 
 @Mod(modid = Sync.MOD_ID, name = Sync.MOD_NAME,
         version = Sync.VERSION,
-        guiFactory = "me.ichun.mods.ichunutil.common.core.config.GenericModGuiFactory",
+        guiFactory = iChunUtil.GUI_CONFIG_FACTORY,
         dependencies = "required-after:ichunutil@[" + iChunUtil.VERSION_MAJOR + ".0.1," + (iChunUtil.VERSION_MAJOR + 1) + ".0.0);after:waila",
         acceptableRemoteVersions = "[" + iChunUtil.VERSION_MAJOR +".0.0," + iChunUtil.VERSION_MAJOR + ".1.0)",
         acceptedMinecraftVersions = iChunUtil.MC_VERSION_RANGE
