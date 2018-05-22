196f78dc3c6292d59362196c29782c90e1427527
Tobias Hotz <tobias.hotz@hotmail.de>
2018-05-22 20:22:00 +0200

    Invalidate the request cache in the skin manager after 15 seconds to avoid values persisting when the session service has been hit with too many requests or is offline

--- src/main/java/me/ichun/mods/sync/client/core/SyncSkinManager.java
+++ src/main/java/me/ichun/mods/sync/client/core/SyncSkinManager.java
@@ -2,8 +2,6 @@ package me.ichun.mods.sync.client.core;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-import com.google.common.collect.Multimap;
-import com.google.common.collect.MultimapBuilder;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import me.ichun.mods.ichunutil.common.core.util.EntityHelper;
@@ -12,10 +10,11 @@ import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.client.network.NetworkPlayerInfo;
 import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
@@ -26,7 +25,7 @@ public class SyncSkinManager {
     //Hold values for a longer time, so they are loaded fast if many TEs with the same player are loaded, or when loading other chunks with the same player
     //Skin loading priority: Cache(fastest), NetworkPlayer(only available when player is only and in same dim as shell, fast), SessionService(slow) and only available if UUID has been set
     private static final Cache<String, ResourceLocation> skinCache = CacheBuilder.newBuilder().expireAfterAccess(15, TimeUnit.MINUTES).build();
-    private static final Multimap<String, Consumer<ResourceLocation>> callbackMap = MultimapBuilder.hashKeys().arrayListValues().build();
+    private static final Cache<String, Set<Consumer<ResourceLocation>>> callbackMap = CacheBuilder.newBuilder().expireAfterWrite(15, TimeUnit.SECONDS).build();
 
     public static void get(String playerName, UUID playerUUID, Consumer<ResourceLocation> callback) {
         ResourceLocation loc = skinCache.getIfPresent(playerName);
@@ -47,18 +46,27 @@ public class SyncSkinManager {
         if (playerUUID == null) return; //Not much we can do here :(
         GameProfile profile = EntityHelper.getGameProfile(playerUUID, playerName);
         synchronized (callbackMap) {
-            if (!callbackMap.containsKey(playerName)) {
+            Set<Consumer<ResourceLocation>> consumers = callbackMap.getIfPresent(playerName);
+            if (consumers == null) {
                 //Make one call per user - again rate limit protection
                 Minecraft.getMinecraft().getSkinManager().loadProfileTextures(profile, (type, location, profileTexture) -> {
                     if (type == MinecraftProfileTexture.Type.SKIN) {
                         synchronized (callbackMap) {
-                            callbackMap.removeAll(playerName).forEach(consumer -> consumer.accept(location));
+                            Set<Consumer<ResourceLocation>> consumerSet = callbackMap.getIfPresent(playerName);
+                            if (consumerSet != null)
+                                consumerSet.forEach(consumer -> consumer.accept(location));
+                            callbackMap.invalidate(playerName);
+                            callbackMap.cleanUp();
                         }
                         skinCache.put(playerName, location);
                     }
                 }, true);
+                HashSet<Consumer<ResourceLocation>> newSet = new HashSet<>();
+                newSet.add(callback);
+                callbackMap.put(playerName, newSet);
+            } else {
+                consumers.add(callback);
             }
-            callbackMap.put(playerName, callback);
         }
     }
 
@@ -66,6 +74,7 @@ public class SyncSkinManager {
     {
         skinCache.invalidateAll();
         skinCache.cleanUp();
-        callbackMap.clear();
+        callbackMap.invalidateAll();
+        callbackMap.cleanUp();
     }
 }
